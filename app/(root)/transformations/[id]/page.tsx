76a4cb1dc822880d90f7c757de9e241b087776c1
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2024-04-02 19:15:16 +0200

    styles, icons, auth ready

---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TransformationsPage = () => {
+  return (
+    <div>Transformations Page</div>
+  )
+}
+
+export default TransformationsPage
+
+// localhost:3000/transformations/[id]
---
+++ app/(root)/transformations/[id]/update/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UpdateTransformationPage = () => {
+	return (
+		<div>UpdateTransformationPage</div>
+	)
+}
+
+export default UpdateTransformationPage
+
+// http://localhost:3000/transformations/123/update
