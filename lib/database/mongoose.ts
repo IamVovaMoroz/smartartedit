37ef073dd740b405b923674e71f076598e205ee7
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2024-04-09 18:59:24 +0200

    ok

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -25,7 +25,7 @@ export const connectToDatabase = async () => {
 		cached.promise ||
 		mongoose.connect(
 			MONGODB_URL, {
-				dbName: "imagecreater", bufferCommands: false
+			dbName: "imagecreater", bufferCommands: false
 		}
 		)
 	cached.conn = await cached.promise;
