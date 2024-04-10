12a109e10f22146131f4ac08e280b19acd95c7a9
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2024-04-10 15:45:30 +0200

    mongoose name

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -25,10 +25,10 @@ export const connectToDatabase = async () => {
 		cached.promise ||
 		mongoose.connect(
 			MONGODB_URL, {
-			dbName: "smartartedit", bufferCommands: false
+			dbName: "imagecreater", bufferCommands: false
 		}
 		)
 	cached.conn = await cached.promise;
 
 	return cached.conn;
-}
+}git 
